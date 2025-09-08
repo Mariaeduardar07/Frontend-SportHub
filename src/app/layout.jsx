bf2f946003c805c52a38533656849327f4ecd459
Maria Eduarda <mariaeduardareisTDS2.senai@gmail.com>
2025-09-08 15:12:04 -0300

    feat: create project

---
+++ src/app/layout.jsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+
+export const metadata = {
+    title: "SportHub",
+    description: "Projeto que reúne informações sobre diversos esportes, trazendo descrições, regras, origens, curiosidades e dados básicos de cada modalidade",
+    icons: {
+        icon: "/icons/favicon.ico",
+    },
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html>
+            <body>{children}</body>
+        </html>
+    );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import style from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={style.container}>
+      <h1 className={style.title}>SportHub</h1>
+    </div>
+  );
+}
